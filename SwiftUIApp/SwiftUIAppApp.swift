7137f51cf5c01c166b673311c20f60efe98d359d
Nurgali Nuraly <nurali.nurgali@gmail.com>
2024-07-01 16:38:55 +0500

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Nurgali on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
